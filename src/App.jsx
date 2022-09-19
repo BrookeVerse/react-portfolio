e545ca688e9c2c008fa3c82bf4ff7abde7cd918f
BrookVerse <brookewaldorf90@gmail.com>
2022-09-19 10:49:37 +0100

    Added a Banner Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.scss';
 
+import NavBar from './components/NabBar/NavBar';
+import Banner from "./components/Banner/Banner";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <Banner />
     </div>
   );
 }
